56f11505222082d75459a078b1d8257d92cbb8ad
dracuthang2000 <nguyenquocthang16022000@gmail.com>
2021-06-25 15:08:45 +0700

    'upDate'

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// let apiQuotes =[];
 
+// Show new Quote
+function newQuote(){
+    // Pick a random quote from api Quotes array
+    const quote = localQuotes[Math.floor(Math.random()*localQuotes.length)];
+    console.log(quote);
+}
+// Get Quotes From API
+// async function getQuotes(){
+//     const apiUrl = 'https://type.fit/api/quotes';
+//     try {
+//         const response = await fetch(apiUrl);
+//         apiQuotes = await response.json();
+//         console.log(apiQuotes[12]);
+//         newQuote();
+//     } catch (error) {
+        
+//     }
+// }
+// getQuotes();
+newQuote();
